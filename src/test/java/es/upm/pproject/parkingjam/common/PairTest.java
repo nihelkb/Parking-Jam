14b24bde3b5b839ba15977f2c6466ccaece018c9
nihelkb <nihelkella@gmail.com>
2023-07-11 14:52:07 +0200

    Removed 'public' modifier

--- src/test/java/es/upm/pproject/parkingjam/common/PairTest.java
+++ src/test/java/es/upm/pproject/parkingjam/common/PairTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class PairTest {
+class PairTest {
     private Pair<Integer, Integer> pair1;
     private Pair<Integer, Integer> pair2;
 
