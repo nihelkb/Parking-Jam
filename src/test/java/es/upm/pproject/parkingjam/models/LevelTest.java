8783c00ccb095aababcb3a902e1f72eed1d4c0fc
nihelkb <nihelkella@gmail.com>
2023-07-12 15:21:26 +0200

    Changed board initialization

--- src/test/java/es/upm/pproject/parkingjam/models/LevelTest.java
+++ src/test/java/es/upm/pproject/parkingjam/models/LevelTest.java
@@ -31,71 +31,18 @@ class LevelTest {
     @Test
     @DisplayName("Reading level 2 file")
     void test1() throws LevelNotFoundException, WrongLevelFormatException{
-        board = new char[8][8];
-        board[0][0] = '+';
-        board[0][1] = '+';
-        board[0][2] = '+';
-        board[0][3] = '+';
-        board[0][4] = '+';
-        board[0][5] = '+';
-        board[0][6] = '+';
-        board[0][7] = '+';
-        board[1][0] = '+';
-        board[1][1] = 'a';
-        board[1][2] = ' ';
-        board[1][3] = ' ';
-        board[1][4] = 'b';
-        board[1][5] = 'b';
-        board[1][6] = 'b';
-        board[1][7] = '+';
-        board[2][0] = '+';
-        board[2][1] = 'a';
-        board[2][2] = ' ';
-        board[2][3] = ' ';
-        board[2][4] = 'c';
-        board[2][5] = ' ';
-        board[2][6] = 'd';
-        board[2][7] = '+';
-        board[3][0] = '+';
-        board[3][1] = '*';
-        board[3][2] = '*';
-        board[3][3] = ' ';
-        board[3][4] = 'c';
-        board[3][5] = 'e';
-        board[3][6] = 'd';
-        board[3][7] = '@';
-        board[4][0] = '+';
-        board[4][1] = 'f';
-        board[4][2] = 'f';
-        board[4][3] = 'f';
-        board[4][4] = ' ';
-        board[4][5] = 'e';
-        board[4][6] = 'd';
-        board[4][7] = '+';
-        board[5][0] = '+';
-        board[5][1] = ' ';
-        board[5][2] = ' ';
-        board[5][3] = 'g';
-        board[5][4] = ' ';
-        board[5][5] = 'j';
-        board[5][6] = 'j';
-        board[5][7] = '+';
-        board[6][0] = '+';
-        board[6][1] = 'h';
-        board[6][2] = 'h';
-        board[6][3] = 'g';
-        board[6][4] = 'i';
-        board[6][5] = 'i';
-        board[6][6] = ' ';
-        board[6][7] = '+';
-        board[7][0] = '+';
-        board[7][1] = '+';
-        board[7][2] = '+';
-        board[7][3] = '+';
-        board[7][4] = '+';
-        board[7][5] = '+';
-        board[7][6] = '+';
-        board[7][7] = '+';
+        char board[][] = {
+            {'+', '+', '+', '+', '+', '+', '+', '+'},
+            {'+', 'a', ' ', ' ', 'b', 'b', 'b', '+'},
+            {'+', 'a', ' ', ' ', 'c', ' ', 'd', '+'},
+            {'+', '*', '*', ' ', 'c', 'e', 'd', '@'},
+            {'+', 'f', 'f', 'f', ' ', 'e', 'd', '+'},
+            {'+', ' ', ' ', 'g', ' ', 'j', 'j', '+'},
+            {'+', 'h', 'h', 'g', 'i', 'i', ' ', '+'},
+            {'+', '+', '+', '+', '+', '+', '+', '+'}
+        };
+
+        this.board = board;
 
         level = new Level(usableLevels + "/level_2.txt");
 
